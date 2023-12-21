d549996c23c0d6d40fb19cba1a8091c5f2fde4e7
Sebastian Restrepo Rico <84477914+srestre84@users.noreply.github.com>
2023-12-21 09:16:03 -0500

    Update registro.js
    .

--- registro.js
+++ registro.js
@@ -1,70 +1,51 @@
-document.addEventListener('DOMContentLoaded', function () {
-    
-    //Se definen las variables  del DOM, La razón por la que se están utilizando ambas formas juntas 
-    //es que getElementById se utiliza para obtener una referencia al elemento principal
-    // con el ID 'registroForm', y luego querySelector se utiliza para buscar un elemento form
-    // específico dentro del respectivo formulario
+document.addEventListener('DOMContentLoaded', () => {
     var registroForm = document.getElementById('registroForm').querySelector('form');
     var loginForm = document.getElementById('loginForm').querySelector('form');
     var mensajeDiv = document.getElementById('mensaje');
 
-    // Configuración de la URL del backend
     var backendUrl = "http://localhost:8080";  // Reemplaza con la URL de tu backend en producción
 
-    // Función para realizar el registro o la autenticación
     function realizarAccion(formData, endpoint) {
         var apiUrl = `${backendUrl}/${endpoint}`;
 
-    //En conjunto, esta solicitud POST está siendo configurada para enviar datos al servidor en formato JSON, y la promesa resultante de fetch es la que se está devolviendo. 
-    //La cadena JSON con los datos del formulario se enviará como parte del cuerpo de la solicitud.   
         return fetch(apiUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(Object.fromEntries(formData)),
-
-     //Luego utilizo las promesas para manejo de errores y analizar el json que envia el servidor       
+            credentials: 'include', // Agrega esto si estás utilizando credenciales
         })
-        .then(function (response) {
+        .then(response => {
             if (!response.ok) {
-                throw new Error('Error en la solicitud al servidor. Estado: ' + response.status);
+                throw new Error(`Error en la solicitud al servidor. Estado: ${response.status}`);
             }
             return response.json();
         })
-        .then(function (data) {
+        .then(data => {
             console.log('Respuesta del servidor:', data);
-
-           // despues de realizar el registro me voy para la url products.html
             window.location.href = 'products.html';
         })
-        .catch(function (error) {
+        .catch(error => {
             console.error('Error en la solicitud:', error);
-            mensajeDiv.textContent = `Error en la conexión con el servidor. Consulta la consola para más detalles (${endpoint}).`;
+            mensajeDiv.textContent = `Error en la conexión con el servidor. Detalles: ${error.message} (${endpoint}).`;
         });
     }
 
-    // Manejar el evento de envío del formulario de registro de usuarios creando un nuevo objeto 
-    // con los datos registrados 
     if (registroForm && mensajeDiv) {
-        registroForm.addEventListener('submit', function (event) {
+        registroForm.addEventListener('submit', event => {
             event.preventDefault();
-           
             var formData = new FormData(registroForm);
             realizarAccion(formData, 'usuarios');
         });
     }
 
-    // Manejar el evento de envío del formulario de inicio de sesión
-    // con los datos registrados 
     if (loginForm && mensajeDiv) {
-        loginForm.addEventListener('submit', function (event) {
+        loginForm.addEventListener('submit', event => {
             event.preventDefault();
-           
             var formData = new FormData(loginForm);
             realizarAccion(formData, 'login');
         });
     }
 });
-
 
