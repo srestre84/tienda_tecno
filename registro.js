3cfb6ab641f758cfaeb729fa4f7f0da6ff0c1abc
Sebastian Restrepo Rico <84477914+srestre84@users.noreply.github.com>
2023-12-26 12:33:23 -0500

    Update registro.js
    se agrega token en el storage para manejarlo en las solicitudes posteriores

--- registro.js
+++ registro.js
@@ -3,38 +3,46 @@ document.addEventListener('DOMContentLoaded', () => {
     var loginForm = document.getElementById('loginForm')?.querySelector('form');
     var mensajeDiv = document.getElementById('mensaje');
 
-    var backendUrl = "http://localhost:8080";  
-
-    function realizarAccion(formData, endpoint) {
+    var backendUrl = "http://localhost:8080";
+    
+    function realizarAccion(formData, endpoint, includeToken = false) {
         var apiUrl = `${backendUrl}/${endpoint}`;
+
+        var headers = {
+            'Content-Type': 'application/json',
+        };
+
+        // Incluir el token en la solicitud si es necesario
+        if (includeToken) {
+            var token = localStorage.getItem("jwtToken");
+            headers['Authorization'] = `Bearer ${token}`;
+        }
 
         return fetch(apiUrl, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+            headers: headers,
             body: JSON.stringify(Object.fromEntries(formData)),
-            credentials: 'include', 
+            credentials: 'include',
         })
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Error en la solicitud al servidor. Estado: ${response.status}`);
             }
-         
+
             const contentType = response.headers.get('content-type');
             if (contentType && contentType.includes('application/json')) {
                 return response.json();
             } else {
-                return {}; 
+                return {};
             }
         })
         .then(data => {
             console.log('Respuesta del servidor:', data);
-       
+
             mensajeDiv.textContent = 'Solicitud exitosa. Redirigiendo...';
             setTimeout(() => {
                 window.location.href = 'products.html';
-            }, 2000); 
+            }, 2000);
         })
         .catch(error => {
             console.error('Error en la solicitud:', error);
@@ -54,9 +62,29 @@ document.addEventListener('DOMContentLoaded', () => {
         loginForm.addEventListener('submit', event => {
             event.preventDefault();
             var formData = new FormData(loginForm);
-            realizarAccion(formData, 'login');
+            realizarAccion(formData, 'login').then(response => {
+                // Almacenar el token en el almacenamiento local después del inicio de sesión
+                localStorage.setItem("jwtToken", response.token);
+            });
         });
     }
+
+    // Función para agregar productos con el token incluido
+    function agregarProducto(event) {
+        event.preventDefault();
+
+        const nombre = document.getElementById('nombre').value;
+        const categoria = document.getElementById('categoria').value;
+
+        realizarAccion({ nombre, categoria }, 'productos', true);
+    }
+
+    // Agregar un listener al formulario o botón de agregar productos
+    var formularioProducto = document.getElementById('formulario-producto');
+    if (formularioProducto) {
+        formularioProducto.addEventListener('submit', agregarProducto);
+    }
 });
+
 
 
